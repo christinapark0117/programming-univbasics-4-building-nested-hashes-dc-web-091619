6f5d0e30705a4267b7d1cbfecb757caf8fac9e07
Christina Park <christinapark0117@gmail.com>
2019-09-13 23:55:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,7 @@ def monopoly_with_third_tier
   monopoly = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {:name => 1, :two => 2, :three => 3, :four => 4}
+      :rent_in_dollars => {:names => 1, :two => 2, :three => 3, :four => 4}
     }
   }
 end
